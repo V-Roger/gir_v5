b3ea407288ef952a9fbb3a810cad21c8e8e707e2
Virgil Roger <roger.virgil@gmail.com>
2021-06-30 10:34:08 +0200

    🔧 Remove next-optimised-images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
-
-module.exports = withPlugins([
-  [optimizedImages, {
-    /* config for next-optimized-images */
-  }],
-], {
+module.exports = {
   reactStrictMode: true,
-});
+};
