6e122f2d5f3e0ea0022c5df1d0e0147846dcab34
Virgil Roger <roger.virgil@gmail.com>
2021-06-30 09:48:16 +0200

    🔧 Update netlify conf to use next plugin

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    /* config for next-optimized-images */
+  }],
+], {
   reactStrictMode: true,
-}
+});
