f217431cbe6e36c44aa7e690678598c4de6605ce
Virgil Roger <roger.virgil@gmail.com>
2021-07-20 10:31:04 +0200

    💩 Revert

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-const nodeExternals = require('webpack-node-externals')
-const { PHASE_PRODUCTION_BUILD, PHASE_PRODUCTION_SERVER } = require('next/constants')
-
-module.exports = (phase) => ({
+module.exports = () => ({
     reactStrictMode: true,
     webpack: (config) => {
         config.module.rules.push(
@@ -13,9 +10,6 @@ module.exports = (phase) => ({
                 }            
             }
         )
-        if (phase === PHASE_PRODUCTION_SERVER) {
-            config.externals = [nodeExternals()]
-        }
 
         return config;
     }
